dde38a413265b65cd820f43d02ad9df850492574
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-05-26 20:39:17 +0200

    Create Input and Modal components

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let value = '';
+  export let placeholder = '';
+  export let id;
+  export let label;
+  export let type = 'text';
+  export let disabled = false;
+  export let required = false;
+
+  export let errors;
+</script>
+
+<div class="form-control mb-2 w-full max-w-lg">
+  <label for={id} class="label pb-1 font-medium">
+    <span class="label-text">{label}</span>
+  </label>
+  <input
+    class="input input-bordered w-full max-w-lg"
+    {type}
+    {placeholder}
+    {required}
+    {disabled}
+    {id}
+    name={id}
+    {value}
+  />
+  {#if errors}
+    {#each errors as error}
+      <label for={id} class="label py-0 pt-1">
+        <span class="label-text-alt text-error">{error}</span>
+      </label>
+    {/each}
+  {/if}
+</div>
---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let label;
+  export let checked;
+</script>
+
+<label for={label}> <slot name="trigger" /></label>
+
+<input type="checkbox" id={label} class="modal-toggle" {checked} />
+<label for={label} class="modal modal-bottom sm:modal-middle">
+  <label class="modal-box">
+    <div class="text-center text-xl font-bold">
+      <slot name="heading" />
+    </div>
+    <div class="my-2">
+      <slot />
+    </div>
+  </label>
+</label>
