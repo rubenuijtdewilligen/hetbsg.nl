aa10a5b1be381804ab218507a12f90d9cb4f8008
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-07-29 17:05:33 +0200

    Remove documents from ES index if removed from PocketBase

--- src/lib/syncPbEs.js
+++ src/lib/syncPbEs.js
@@ -50,15 +50,55 @@ async function expandRelations(item) {
 }
 
 export const syncMediaItems = async () => {
-  console.log('Start syncing media_items to Elasticsearch...');
-  console.log('');
+  console.log('Start syncing media_items to Elasticsearch...\n');
 
   const mediaItems = await pb.collection('media_items').getFullList();
+  const pocketbaseIds = new Set(mediaItems.map((item) => item.id));
+
+  const esItems = await es.search({
+    index: 'media_items',
+    scroll: '1m',
+    size: 1000,
+    body: {
+      query: {
+        match_all: {}
+      }
+    }
+  });
+
+  const esIds = new Set();
+  let scrollId = esItems._scroll_id;
+  let hits = esItems.hits.hits;
+
+  while (hits.length > 0) {
+    for (const hit of hits) {
+      esIds.add(hit._id);
+    }
+
+    const scrollRes = await es.scroll({
+      scroll_id: scrollId,
+      scroll: '1m'
+    });
+
+    scrollId = scrollRes._scroll_id;
+    hits = scrollRes.hits.hits;
+  }
+
+  const toDelete = [...esIds].filter((id) => !pocketbaseIds.has(id));
+  for (const id of toDelete) {
+    try {
+      await es.delete({
+        index: 'media_items',
+        id
+      });
+      console.log(`Deleted from Elasticsearch: ${id}`);
+    } catch (err) {
+      console.error(`Error deleting ${id}:`, err);
+    }
+  }
 
   for (const item of mediaItems) {
     const expanded = await expandRelations(item);
-
-    console.log(expanded);
 
     const doc = {
       pocketbaseId: item.id,
@@ -74,20 +114,17 @@ export const syncMediaItems = async () => {
       file: getFileUrl(item.collectionId, item.id, item.file)
     };
 
-    console.log(doc);
-
     try {
       await es.index({
         index: 'media_items',
         id: item.id,
         document: doc
       });
-      console.log(`Indexed: ${item.title}.`);
+      console.log(`Indexed: ${item.title}`);
     } catch (err) {
       console.error(`Error indexing ${item.title}:`, err);
     }
   }
 
-  console.log('');
-  console.log(`Indexed ${mediaItems.length} media items.`);
+  console.log(`\nDone syncing. Indexed: ${mediaItems.length}. Deleted: ${toDelete.length}.`);
 };
