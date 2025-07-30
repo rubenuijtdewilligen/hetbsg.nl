5e6b5529023d0daf0f583274bb7115b614315d99
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-07-30 14:19:08 +0200

    Oops mixed up variables

--- src/routes/(pages)/+layout.svelte
+++ src/routes/(pages)/+layout.svelte
@@ -4,7 +4,7 @@
   export let data;
 
   $: path = $page.url.pathname;
-  $: title = data.pageTitles.find((page) => path.startsWith(page.path))?.title || 'BSG';
+  $: title = data.pageTitles.find((page) => page.path.startsWith(path))?.title || 'BSG';
 </script>
 
 <div class="hero" style="background-image: url(/vlag.png);">
